f5ec1c927e79ab585c109fb261a358d1e56c371b
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-23 22:33:36 +0900

    6-72

--- myfirstclass.py
+++ myfirstclass.py
@@ -1,20 +1,28 @@
 class Person(object):
+
+    num_legs = 2
+    count = 0
+
     # constructor (__new__)
     def __init__(self, name , age, gender):
         self.name = name
         self.age = age
         self.gender = gender
+        Person.count += 1
 
     def walk(self):
-        print(f"{self.name} is walking.")
+        print(f"{self.name} is walking... with {Person.num_legs} legs!")
 
     def run(self):
-        print(f"{self.name} is running.")
+        print(f"{self.name} is running... with {Person.num_legs} legs!")
 
 
 john = Person("John", 28, 'male')
+print(Person.count)
 taro = Person("Taro", 18, 'male')
+print(Person.count)
 emma = Person("Emma", 40, 'female')
+print(Person.count)
 
 # インスタンス変数
 # 「.」に続けてアクセス可能
@@ -32,3 +40,18 @@ print(taro.gender)
 john.walk()
 emma.walk()
 john.run()
+
+print(john.num_legs)
+print(taro.num_legs)
+
+print(Person.num_legs)
+print("Person.num_legs = 3を実行")
+Person.num_legs = 3
+print(john.num_legs)
+print(taro.num_legs)
+
+print("john.num_legs = 4を実行")
+john.num_legs = 4
+print(john.num_legs)
+print(taro.num_legs)
+print(emma.num_legs)
