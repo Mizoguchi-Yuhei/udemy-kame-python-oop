48d9103ebb5f43b4a7c247df63649d65d0a96dce
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-26 19:23:02 +0900

    6-74-1

---
+++ staticmethod.py
@@ -0,0 +1,14 @@
+class MyClass:
+
+    def mymethod(self):
+        print("This is normal method from {}.".format(self))
+
+    @staticmethod
+    def mystaticmethod():
+        print("This is staticmethod!")
+
+
+c = MyClass()
+c.mymethod()
+MyClass.mystaticmethod()
+# c.mystaticmethod()
