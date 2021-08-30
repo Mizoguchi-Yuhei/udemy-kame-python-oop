27c3082efe90ec77e21317febedb19042f1ff62b
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-30 23:25:24 +0900

    6-77

---
+++ getter_setter.py
@@ -0,0 +1,29 @@
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self._age = age
+
+    def get_age(self):
+        print("get_age called!")
+        return self._age
+
+    def set_age(self, age):
+        print("set_age called!")
+        if age < 0:
+            print("0以上の値を入れてください")
+        else:
+            self._age
+
+    age = property(get_age, set_age)
+
+john = Person("John", 10)
+print(john.age)
+john.age = -10
+
+# print(john.age)
+# print(john.get_age())
+# john.age = 15
+# john.set_age(-20)
+# print(john.age)
+# print(john.get_age())
